14da13b4824e58a4163e7b63ca73fcb952bd0423
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-04-12 21:05:58 +0900

    fix: Fixed ebi position

--- ebiFly/Title/GameScene.swift
+++ ebiFly/Title/GameScene.swift
@@ -11,49 +11,30 @@ import GameplayKit
 import UIKit
 
 class GameScene: SKScene {
-    let startLabel = SKLabelNode(fontNamed: "Verdana-bold")
-    let ebiSprite = SKSpriteNode(imageNamed: "ebiAnimation01")
-    let ebiSprite2 = SKSpriteNode(imageNamed: "ebiAnimation04")
-    let ebiSprite3 = SKSpriteNode(imageNamed: "ebiAnimation04")
+    lazy var startLabel = SKLabelNode(fontSize: 70, text: "START", pos: CGPoint(x: width/2, y: height/7))
+    lazy var ebiSprite =  SKSpriteNode(image: "ebiAnimation01", pos: CGPoint(x: width/2, y: height/7), size: CGSize(width: width/3, height: width/9))
+    lazy var ebiSprite2 = SKSpriteNode(image: "ebiAnimation04", pos: CGPoint(x: width/2, y: height/1.5), size: CGSize(width: width/2, height: width/5))
+    lazy var ebiSprite3 = SKSpriteNode(image: "ebiAnimation04", pos: CGPoint(x: width/6, y: height/8), size: CGSize(width: width/3, height: width/9))
+    var width: CGFloat!
+    var height: CGFloat!
+    
+    // MARK: LifeCycle
     
     override func didMove(to view: SKView) {
+        self.anchorPoint = CGPoint(x: 0, y: 0)
         self.backgroundColor = SKColor(red: 36/255, green: 139/255, blue: 255/255, alpha: 1)
-        startLabel.text = "START"
-        startLabel.fontSize = 150
-        startLabel.position = CGPoint(x:0, y:-250)
+        
+        width = self.view!.frame.width
+        height =  self.view!.frame.height
+        
         self.addChild(startLabel)
-        
-        ebiSprite.position  = CGPoint(x: 0, y: 0)
-        ebiSprite.size = CGSize(width: 300, height: 100)
         self.addChild(ebiSprite)
-        let playerAnime = SKAction.animate(
-            with: [SKTexture(imageNamed: "ebiAnimation02"),SKTexture(imageNamed: "ebiAnimation03"),SKTexture(imageNamed: "ebiAnimation02"), SKTexture(imageNamed: "ebiAnimation01"),SKTexture(imageNamed: "ebiAnimation02"), SKTexture(imageNamed: "ebiAnimation03")],
-            timePerFrame: 0.2)
-        ebiSprite.run(SKAction.repeatForever(playerAnime))
-        let moveAction = SKAction.moveBy(x: 80.0, y: 0, duration: 1.0)
-        ebiSprite.run(SKAction.repeatForever(moveAction))
-        
-        
-        ebiSprite2.position  = CGPoint(x: self.view!.frame.width/2, y: self.view!.frame.height/2)
-        ebiSprite2.size = CGSize(width: 200, height: 70)
         self.addChild(ebiSprite2)
-        let playerAnime2 = SKAction.animate(
-            with: [SKTexture(imageNamed: "ebiAnimation05"),SKTexture(imageNamed: "ebiAnimation06"),SKTexture(imageNamed: "ebiAnimation05"), SKTexture(imageNamed: "ebiAnimation04"),SKTexture(imageNamed: "ebiAnimation05"), SKTexture(imageNamed: "ebiAnimation06")],
-            timePerFrame: 0.2)
-        ebiSprite2.run(SKAction.repeatForever(playerAnime2))
-        let moveAction2 = SKAction.moveBy(x: -40.0, y: 0, duration: 1.0)
-        ebiSprite2.run(SKAction.repeatForever(moveAction2))
-
-        ebiSprite3.position  = CGPoint(x: self.view!.frame.width/6, y: self.view!.frame.height/8)
-        ebiSprite3.size = CGSize(width: 120, height: 30)
         self.addChild(ebiSprite3)
-        let playerAnime3 = SKAction.animate(
-            with: [SKTexture(imageNamed: "ebiAnimation05"),SKTexture(imageNamed: "ebiAnimation06"),SKTexture(imageNamed: "ebiAnimation05"), SKTexture(imageNamed: "ebiAnimation04"),SKTexture(imageNamed: "ebiAnimation05"), SKTexture(imageNamed: "ebiAnimation06")],
-            timePerFrame: 0.2)
-        ebiSprite3.run(SKAction.repeatForever(playerAnime3))
-        let moveAction3 = SKAction.moveBy(x: -200.0, y: 0, duration: 1.0)
-        ebiSprite3.run(SKAction.repeatForever(moveAction3))
+
+        ebiAnimation()
     }
+    
     override func update(_ currentTime: TimeInterval) {
         if ebiSprite.position.x > (self.view?.frame.width)! + 100 {
             ebiSprite.position.x = -self.view!.frame.width
@@ -67,6 +48,32 @@ class GameScene: SKScene {
             ebiSprite3.position.x = self.view!.frame.width
         }
     }
+    
+    // MARK: - PrivateMethod
+    
+    private func ebiAnimation() {
+        let moveRightAnimation = SKAction.animate(
+            with: [SKTexture(imageNamed: "ebiAnimation02"),SKTexture(imageNamed: "ebiAnimation03"),SKTexture(imageNamed: "ebiAnimation02"), SKTexture(imageNamed: "ebiAnimation01")],
+            timePerFrame: 0.2)
+        let moveLeftAnimation = SKAction.animate(
+            with: [SKTexture(imageNamed: "ebiAnimation05"),SKTexture(imageNamed: "ebiAnimation06"),SKTexture(imageNamed: "ebiAnimation05"), SKTexture(imageNamed: "ebiAnimation04")],
+            timePerFrame: 0.2)
+        
+        ebiSprite.run(SKAction.repeatForever(moveRightAnimation))
+        let moveAction = SKAction.moveBy(x: 80.0, y: 0, duration: 1.0)
+        ebiSprite.run(SKAction.repeatForever(moveAction))
+        
+        ebiSprite2.run(SKAction.repeatForever(moveLeftAnimation))
+        let moveAction2 = SKAction.moveBy(x: -40.0, y: 0, duration: 1.0)
+        ebiSprite2.run(SKAction.repeatForever(moveAction2))
+        
+        ebiSprite3.run(SKAction.repeatForever(moveLeftAnimation))
+        let moveAction3 = SKAction.moveBy(x: -200.0, y: 0, duration: 1.0)
+        ebiSprite3.run(SKAction.repeatForever(moveAction3))
+    }
+    
+    // MARK: - Event
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let location = touches.first?.location(in: self){
             let touchNode = self.atPoint(location)
