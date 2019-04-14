85d7a0f0a28d617bdff64cc9fe9ef024f6970f9e
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-04-14 20:41:25 +0900

    fix: Fixed gameClearScleen layout

--- ebiFly/GameClear/GameClearScene.swift
+++ ebiFly/GameClear/GameClearScene.swift
@@ -11,13 +11,13 @@ import SpriteKit
 class GameClearScene: SKScene {
     let score: Int
     lazy var scoreLabel: SKLabelNode! = {
-        return SKLabelNode(font: "Verdana-bold", fontSize: 150, text: "\(score)m", pos: CGPoint(x:self.frame.width/2, y:self.frame.height - self.frame.height/3))
+        return SKLabelNode(font: "Verdana-bold", fontSize: 50, text: "\(score)m", pos: CGPoint(x:self.frame.width/2, y:self.frame.height - self.frame.height/5))
     }()
     lazy var retrayLabel: SKLabelNode! = {
-       return SKLabelNode(fontSize: 100, text: "もう一度遊ぶ", pos: CGPoint(x: self.frame.width/2, y: self.frame.height/4))
+       return SKLabelNode(fontSize: 50, text: "もう一度遊ぶ", pos: CGPoint(x: self.frame.width/2, y: self.frame.height/6))
     }()
     lazy var clearImage:SKSpriteNode! = {
-        return SKSpriteNode(image: "clearimage", pos: CGPoint(x: self.frame.width/2, y: self.frame.height/2), size: CGSize(width: self.view!.frame.width, height: self.view!.frame.width))
+        return SKSpriteNode(image: "clearimage", pos: CGPoint(x: self.frame.width/2, y: self.frame.height/2), size: CGSize(width: self.view!.frame.width/1.5, height: self.view!.frame.width/1.5))
     }()
 
     // MARK: - Initializer
